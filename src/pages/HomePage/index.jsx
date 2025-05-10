125aac0fc3740921fd88415edef56c11e8b885e7
Zuzana Ilečková <yes>
2025-05-10 16:52:16 +0200

    změna k vybírání trička a jeho podobě

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,26 @@
 import { TShirt } from '../../components/TShirt';
 import { TShirtSetup } from '../../components/TShirtSetup';
 import './style.css';
+import { useState } from 'react';
 
 export const HomePage = () => {
+  const [type, setType] = useState('normalShortSleeve');
+  const [color, setColor] = useState('');
+  const [print, setPrint] = useState('');
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt type={type} color={color} print={print} />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup
+            onColorChange={setColor}
+            onPrintChange={setPrint}
+            onTypeChange={setType}
+          />
         </div>
       </div>
     </div>
